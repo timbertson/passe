c4e05bd83a04ecacf6f409c1403c36ba677f9555
Tim Cuthbertson <tim@gfxmonk.net>
2014-06-23 09:23:16 +1000

    forgot to add re.ml

---
+++ src/common/re.ml
@@ -0,0 +1,18 @@
+(* cross-env abtraction over js Regexp / native Str *)
+
+IFDEF JS THEN
+	module Str = struct
+		let regexp = Regexp.regexp
+		let regexp_string = Regexp.regexp_string
+		let string_match r s = Regexp.string_match r s 0 |> Option.is_some
+		let contains r s = Regexp.search_forward r s 0 |> Option.is_some
+	end
+ELSE
+	module Str = struct
+		let regexp = Str.regexp
+		let regexp_string = Str.regexp_string
+		let string_match r s = Regexp.string_match r s 0
+		let contains r s = try Regexp.search_forward r s 0; true with Not_found -> false
+	end
+END
+
