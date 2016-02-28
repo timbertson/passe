33ff2c3ff45fe5c4178d469a4054a1b06f77278c
Tim Cuthbertson <tim@gfxmonk.net>
2016-02-28 22:51:54 +1100

    fix manifest target

--- gup/_build/build_type.sh
+++ gup/_build/build_type.sh
@@ -2,6 +2,10 @@
 self="${BASH_SOURCE[0]}"
 gup -u "$self"
 build_dir="$(basename "$(dirname "$self")")"
+
+# XXX workaround for gup bug, fixed in 0.5.4
+build_dir="$(pwd | grep -E -o '_build[^/]*?' | tail -n 1)"
+
 case $build_dir in
 	_build) build_type="dev";;
 	_build.prod) build_type="prod";;
