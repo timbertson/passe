7699645a16c760ff11a6e40ef7d9f8d8c145aac1
Tim Cuthbertson <tim@gfxmonk.net>
2016-08-08 19:05:29 +1000

    fix test logging

--- src/native/test/test_common.ml
+++ src/native/test/test_common.ml
@@ -7,12 +7,34 @@ module J = Json_ext
 
 (* module-wide setup *)
 let (>::) desc test =
-	let initial_writer = !Logging.current_writer in
+	let initial_reporter = Logs.reporter () in
 	let setup ctx =
-		Logging.current_writer := (fun _dest str -> logf ctx `Info "%s" str)
+		let ppf, flush =
+			let b = Buffer.create 255 in
+			let flush () = let s = Buffer.contents b in Buffer.clear b; s in
+			Format.formatter_of_buffer b, flush
+		in
+
+		let report_fn src level ~over k msgf =
+			let l = match level with
+				| Logs.Error -> `Error
+				| Logs.Warning -> `Warning
+				| Logs.App | Logs.Info | Logs.Debug -> `Info
+			in
+			let k _ =
+				logf ctx l "%s" (flush ());
+				over ();
+				k ()
+			in
+			msgf @@ fun ?header ?tags fmt ->
+			Format.kfprintf k ppf ("@[" ^^ fmt ^^ "@]@.")
+		in
+		Logs.(set_reporter { report = report_fn })
+
+
 	in
 	let teardown () _ =
-		Logging.current_writer := initial_writer
+		Logs.set_reporter initial_reporter
 	in
 	desc >:: (fun ctx ->
 		bracket setup teardown ctx;
