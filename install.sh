33c6711cd3012afb73a89525a56e2973cfabd23a
Tim Cuthbertson <tim@gfxmonk.net>
2018-06-04 13:47:05 +1000

    update install.sh

--- install.sh
+++ install.sh
@@ -2,21 +2,27 @@
 set -eu
 base="$(dirname "$0")"
 
-installed=0
-targets="$(find "$base/"_build/default -maxdepth 1 -type f -name '*.install')"
-if [ -z "$targets" ]; then
-	echo "Error: nothing to install - try building an .install file first"
-	exit 1
+PREFIX="$(realpath "$1")"
+shift
+
+install_base="_build/default"
+
+if [ "$#" -eq 0 ]; then
+	targets="$(cd "$base/$install_base" && find -maxdepth 1 -type f -name '*.install')"
+	if [ -z "$targets" ]; then
+		echo "Error: nothing to install - try building an .install file first"
+		exit 1
+	fi
+else
+	targets="$@"
 fi
 
-echo "Installing: $targets"
-
-mkdir -p "$1"
-PREFIX="$(cd "$1" && pwd)"
-cd "$base"
-shift
+mkdir -p "$PREFIX"
+prefix="$(realpath "$PREFIX")"
 
 for target in $targets; do
 	echo "Installing: $target"
-	opam-installer "$target" --prefix="$PREFIX" "$@"
+	pwd
+	cd "$base"
+	opam-installer "$install_base/$target" --prefix="$PREFIX"
 done
